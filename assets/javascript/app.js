245855858a4f575988536fb5c9c354de694723c1
Kalpesh Patel <kapu.9899@gmail.com>
2017-10-23 21:43:09 -0500

    Initial UI Proposal
    Initial UI Proposal. Still working on it to make it better.

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+	$(".container").on("click",".btn-sports", function(){
+		var sportName = $(this).attr("data-name");
+		var url = "https://api.seatgeek.com/2/events?q=" + sportName + "&client_id=OTM3ODIzNHwxNTA4ODAxNzUyLjY0";
+		$.ajax({
+			url: url,
+			type: 'GET',
+		})
+		.done(function(response) {
+			console.log(response);
+		});
+	});
+});
