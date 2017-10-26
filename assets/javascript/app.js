7b41b1896dabf9336d4ccba34030662bf15bd8d3
Kalpesh Patel <kapu.9899@gmail.com>
2017-10-26 17:57:23 -0500

    tried displaying response on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
 
 
 $(document).ready(function(){
+	
+	/* Function to get user IP address */
+	/* Help to find current location */
+	$.get("http://ipinfo.io", function(response) {
+    	alert(response.ip);
+	}, "jsonp");
+
 	$(".container").on("click",".btn-sports", function(){
 		var sportName = $(this).attr("data-name");
 		var url = "https://api.seatgeek.com/2/events?q=" + sportName + "&client_id=OTM3ODIzNHwxNTA4ODAxNzUyLjY0";
@@ -10,6 +17,37 @@ $(document).ready(function(){
 		})
 		.done(function(response) {
 			console.log(response);
+			// console.log(response);
+			$(".primary-content").html('');
+			for(var i=0; i<response.events.length; i++ ){
+				var card = $("<div class='card'></div>");
+				var cardHeader = $("<div class='card-header'></div>");
+				var cardBody = $("<div class='card-body'></div>");
+				var row = $("<div class='row'></div>");
+
+				var innerRow = $("<div class='row'></div>");
+
+				var imageColumn = $("<div class='col-md-3'></div>");
+				var img = $("<img width='100px' height='100px' src='" + response.events[i].performers[0].image + "'>");
+				var contentColumn = $("<div class='col-md-9'></div>");
+
+				cardHeader.html(response.events[i].title);
+				
+				imageColumn.append(img);
+				contentColumn.html(response.events[i].venue.address);
+				contentColumn.html(response.events[i].venue.display_location);
+
+				innerRow.append(imageColumn);
+				innerRow.append(contentColumn);
+
+				cardBody.append(innerRow);
+
+				card.append(cardHeader);
+				card.append(cardBody);
+
+				$(".primary-content").append(card);
+			}
 		});
 	});
+
 });
