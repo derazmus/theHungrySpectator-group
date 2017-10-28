061b7842436f4a806b10ff96a22945886a8a25b3
Kelvin Tiongson <kelvintiongson30@gmail.com>
2017-10-28 06:38:31 -0500

    added the ajax to display event info when button in side bar is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,21 @@
 
-$(".food-btn").on("click", function() {
-    var zkey = "312c4d4c34ef87c50671cf222cece7f5";
-    var foodName = $(this).attr("data-name");
+// $(".food-btn").on("click", function() {
+//     var zkey = "312c4d4c34ef87c50671cf222cece7f5";
+//     var foodName = $(this).attr("data-name");
 
-    // contains the latitude and longitude of Evanston, IL
-    var searchURL = "https://developers.zomato.com/api/v2.1/search?apikey=" + 
-        zkey +"&count=10&lat=42.0451&lon=-87.6877&q=" + foodName;
+//     // contains the latitude and longitude of Evanston, IL
+//     var searchURL = "https://developers.zomato.com/api/v2.1/search?apikey=" + 
+//         zkey +"&count=10&lat=42.0451&lon=-87.6877&q=" + foodName;
 
-    $.ajax({
-        url: searchURL,
-        type: 'GET'
-    }).done(function (response) {
-        console.log(response);
+//     $.ajax({
+//         url: searchURL,
+//         type: 'GET'
+//     }).done(function (response) {
+//         console.log(response);
         
-        $(".primary-content").html('');
+//         $(".primary-content").html('');
 
-        for(var i = 0; i < response.restaurants.length; i++) {
-                var card = $("<div class='card'></div>");
-                var cardHeader = $("<div class='card-header'></div>");
-                var cardBody = $("<div class='card-body'></div>");
-                var row = $("<div class='row'></div>");
-
-                var innerRow = $("<div class='row'></div>");
-
-                var imageColumn = $("<div class='col-md-3'></div>");
-                var img = $("<img width='100px' height='100px' src='" + response.restaurants[i].restaurant.featured_image + "'>");
-                var contentColumn = $("<div class='col-md-9'></div>");
-
-                cardHeader.html(response.restaurants[i].restaurant.name);
-                imageColumn.append(img);
-                
-                // contentColumn.html(response.restaurants[i].restaurant.cuisines);
-                contentColumn.html(response.restaurants[i].restaurant.location.address);
-                // contentColumn.html(response.restaurants[i].restaurant.average_cost_for_two);
-
-                innerRow.append(imageColumn);
-                innerRow.append(contentColumn);
-
-                cardBody.append(innerRow);
-
-                card.append(cardHeader);
-                card.append(cardBody);
-
-                $(".primary-content").append(card);           
-        }
-    })
-});
-
-
-// $(document).ready(function(){
-    
-//     /* Function to get user IP address */
-//     /* Help to find current location */
-//     $.get("http://ipinfo.io", function(response) {
-//         alert(response.ip);
-//     }, "jsonp");
-
-//     $(".container").on("click",".btn-sports", function(){
-//         var sportName = $(this).attr("data-name");
-//         var url = "https://api.seatgeek.com/2/events?q=" + sportName + "&client_id=OTM3ODIzNHwxNTA4ODAxNzUyLjY0";
-//         $.ajax({
-//             url: url,
-//             type: 'GET',
-//         })
-//         .done(function(response) {
-//             console.log(response);
-//             // console.log(response);
-//             $(".primary-content").html('');
-//             for(var i=0; i<response.events.length; i++ ){
+//         for(var i = 0; i < response.restaurants.length; i++) {
 //                 var card = $("<div class='card'></div>");
 //                 var cardHeader = $("<div class='card-header'></div>");
 //                 var cardBody = $("<div class='card-body'></div>");
@@ -76,13 +24,15 @@ $(".food-btn").on("click", function() {
 //                 var innerRow = $("<div class='row'></div>");
 
 //                 var imageColumn = $("<div class='col-md-3'></div>");
-//                 var img = $("<img width='100px' height='100px' src='" + response.events[i].performers[0].image + "'>");
+//                 var img = $("<img width='100px' height='100px' src='" + response.restaurants[i].restaurant.featured_image + "'>");
 //                 var contentColumn = $("<div class='col-md-9'></div>");
 
-//                 cardHeader.html(response.events[i].title);
+//                 cardHeader.html(response.restaurants[i].restaurant.name);
 //                 imageColumn.append(img);
-//                 contentColumn.html(response.events[i].venue.address);
-//                 contentColumn.html(response.events[i].venue.display_location);
+                
+//                 // contentColumn.html(response.restaurants[i].restaurant.cuisines);
+//                 contentColumn.html(response.restaurants[i].restaurant.location.address);
+//                 // contentColumn.html(response.restaurants[i].restaurant.average_cost_for_two);
 
 //                 innerRow.append(imageColumn);
 //                 innerRow.append(contentColumn);
@@ -92,7 +42,58 @@ $(".food-btn").on("click", function() {
 //                 card.append(cardHeader);
 //                 card.append(cardBody);
 
-//                 $(".primary-content").append(card);
-//             }
-//         });
-//     });
+//                 $(".primary-content").append(card);           
+//         }
+//     })
+// });
+
+
+$(document).ready(function(){
+    
+    /* Function to get user IP address */
+    /* Help to find current location */
+    $.get("http://ipinfo.io", function(response) {
+        alert(response.ip);
+    }, "jsonp");
+
+    $(".container").on("click",".btn-sports", function(){
+        var sportName = $(this).attr("data-name");
+        var url = "https://api.seatgeek.com/2/events?q=" + sportName + "&client_id=OTM3ODIzNHwxNTA4ODAxNzUyLjY0";
+        $.ajax({
+            url: url,
+            type: 'GET',
+        })
+        .done(function(response) {
+            console.log(response);
+            // console.log(response);
+            $(".primary-content").html('');
+            for(var i=0; i<response.events.length; i++ ){
+                var card = $("<div class='card'></div>");
+                var cardHeader = $("<div class='card-header'></div>");
+                var cardBody = $("<div class='card-body'></div>");
+                var row = $("<div class='row'></div>");
+
+                var innerRow = $("<div class='row'></div>");
+
+                var imageColumn = $("<div class='col-md-3'></div>");
+                var img = $("<img width='100px' height='100px' src='" + response.events[i].performers[0].image + "'>");
+                var contentColumn = $("<div class='col-md-9'></div>");
+
+                cardHeader.html(response.events[i].title);
+                imageColumn.append(img);
+                contentColumn.html(response.events[i].venue.address);
+                contentColumn.html(response.events[i].venue.display_location);
+
+                innerRow.append(imageColumn);
+                innerRow.append(contentColumn);
+
+                cardBody.append(innerRow);
+
+                card.append(cardHeader);
+                card.append(cardBody);
+
+                $(".primary-content").append(card);
+            }
+        });
+    });
+});
